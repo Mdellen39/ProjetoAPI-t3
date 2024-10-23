4e97e42e0bc898f703219ca70b35fb6a6aae8594
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-10-22 22:27:43 -0300

    01
    01

--- src/pessoa/dto/criaPessoa.dto.ts
+++ src/pessoa/dto/criaPessoa.dto.ts
@@ -1,19 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsEmail, IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
 
-
-export class CriaPessoaDTO{
+export class CriaPessoaDTO {
     @IsString()
-    @IsNotEmpty({message: "nome não pode ser vazio"})
+    @IsNotEmpty({ message: "nome não pode ser vazio" })
     @ApiProperty({
         example: "Joao",
         description: "Nome do usuário, deve ser informado um texto contendo o nome"
     })
     NOME: string;
 
-
     @IsDate()
-    @IsNotEmpty({message: "Nascimento não pode ser vazio"})
+    @IsNotEmpty({ message: "Nascimento não pode ser vazio" })
     @ApiProperty({
         example: "2010-02-18",
         description: "Data de nascimento do usuário, deve ser informado como date(2010-02-18)"
@@ -21,7 +19,7 @@ export class CriaPessoaDTO{
     NASCIMENTO: Date;
 
     @IsString()
-    @IsNotEmpty({message: "Pais não pode ser vazio"})
+    @IsNotEmpty({ message: "Pais não pode ser vazio" })
     @ApiProperty({
         example: "BRASIL",
         description: "Pais do usuário, deve ser informado um texto apenas o nome do pais"
--- src/pessoa/pessoa.controller.ts
+++ src/pessoa/pessoa.controller.ts
@@ -1,39 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common"
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { RetornoCadastroDTO, RetornoObjDTO } from "src/dto/retorno.dto";
 import { PessoaService } from "./pessoa.service";
 import { PESSOA } from "./pessoa.entity";
 import { CriaPessoaDTO } from "./dto/criaPessoa.dto";
 import { AlteraPessoaDTO } from "./dto/atualizaPessoa.dto";
+// import { CriaPessoaDTO } from "./dtopessoa/criaPessoa.dto";
+// import { AlteraPessoaDTO } from "./dtopessoa/atualizaPessoa.dto";
 
 @Controller('/pessoa')
-export class PessoaController{
-    constructor(private readonly pessoaService: PessoaService){
-             
-    }
+export class PessoaController {
+    constructor(private readonly pessoaService: PessoaService) {}
 
     @Get('')
-    async listar(): Promise<PESSOA[]>{
+    async listar(): Promise<PESSOA[]> {
         return this.pessoaService.listar();
     }
 
     @Post('')
-    async criaPessoa(@Body() dados: CriaPessoaDTO): Promise<RetornoCadastroDTO>{        
-        return this.pessoaService.inserir(dados)        
+    async criaPessoa(@Body() dados: CriaPessoaDTO): Promise<RetornoCadastroDTO> {
+        return this.pessoaService.inserir(dados);        
     }
 
     @Put(':id')
-    async alterarPessoa(@Body() dados: AlteraPessoaDTO,@Param('id') id: string): Promise<RetornoCadastroDTO>{        
-        return this.pessoaService.alterar(id,dados)        
+    async alterarPessoa(@Body() dados: AlteraPessoaDTO, @Param('id') id: string): Promise<RetornoCadastroDTO> {
+        return this.pessoaService.alterar(id, dados);        
     }
-    
+
     @Get('ID-:id')
-    async listarID(@Param('id') id: string): Promise<PESSOA>{
+    async listarID(@Param('id') id: string): Promise<PESSOA> {
         return this.pessoaService.localizarID(id);
     }
 
     @Delete('remove-:id')
-    async removePessoa(@Param('id') id: string): Promise<RetornoObjDTO>{
+    async removePessoa(@Param('id') id: string): Promise<RetornoObjDTO> {
         return this.pessoaService.remover(id);
     }    
-
 }
